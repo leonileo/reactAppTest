ab7759d1a5e3f3bfb3821b5da186f327e625a9a1
leonileo <leolittleprogrammer@gmail.com>
2024-06-15 21:27:56 +0300

    check

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,59 +6,48 @@ import { useEffect, useState } from "react"
 const Home = () => {
   const [success, setSuccess] = useState(false);
   const [mess, setMess] = useState("");
+  const [loading, setLoading] = useState(false);
 
-//   const getHello = async () => {
-    // const res =  await fetch('https://astro-backend-yj6z.onrender.com/', {
-    //   method: "GET", // *GET, POST, PUT, DELETE, etc.
-    //   mode: "cors", // no-cors, *cors, same-origin
-    //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    //   credentials: "same-origin", // include, *same-origin, omit
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     // 'Content-Type': 'application/x-www-form-urlencoded',
-    //   },
-    //   redirect: "follow", // manual, *follow, error
-    //   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    //   // body: JSON.stringify(data), // body data type must match "Content-Type" header
-    // });
-//     const res = await fetch("https://astro-backend-yj6z.onrender.com/");
-
-//     console.log(res)
-//   }
-//   getHello();
-// await fetch('https://astro-backend-yj6z.onrender.com/')
-    useEffect(() => {
-      axios.get('https://astro-backend-yj6z.onrender.com/')
-      .then(mess => {
+  const getHello = async () => {
+    setLoading(true)
+    const res = await fetch("https://astro-backend-yj6z.onrender.com/");
+    axios.get('https://astro-backend-yj6z.onrender.com/')
+    .then(mess => {
         setSuccess(true)
         setMess(mess.data)
-      }).catch(()=>{
+        setLoading(false)
+    }).catch(()=>{
+        setLoading(false)
         setSuccess(false)
-      })
-    },[])
+    })
+  }
   return (
-      <div 
-        //   initial={{ x: "10%", opacity:0 }}
-        //   animate={{
-        //     x: "0%", opacity:1
-        // }}
+      <motion.div 
+          initial={{ x: "10%", opacity:0 }}
+          animate={{
+            x: "0%", opacity:1
+        }}
           className='h-full bg-gradient-to-t bg-lime-50 flex justify-center items-center'>
         <div className=' grid justify-center'>
           <p className='font-extralight text-[50px] text-center'> This is a test website! </p>
           <p>Edited now!</p>
-          <Message mess={mess}/>
+          <Message mess={mess}/> 
           <div className='my-5 text-center'>
-            <button 
-            // initial={{y: "-100%", opacity:0}} 
-            // animate={{y: "0", opacity: 1, transition: {duration: 0}}} 
+            <motion.button 
+            initial={{y: "-100%", opacity:0}} 
+            animate={{y: "0", opacity: 1, transition: {duration: 0}}} 
             className='bg-blue-400 hover:bg-blue-500 transition-all text-white p-3 w-[150px] text-[20px] rounded-full'
-            // onClick={getHello}
+            onClick={getHello}
             > 
-              Say hi
-            </button>
+            {loading ?
+                "Loading..."
+            :
+              "Say hi"
+            }
+            </motion.button>
           </div>
         </div>
-        </div>
+        </motion.div>
   )
 }
 
